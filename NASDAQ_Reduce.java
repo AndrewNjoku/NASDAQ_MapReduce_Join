77298a6030dc692dd1c169a7f387c74c31441603
Andria Njoku <andrianjoku@gmail.com>
2018-11-12 15:58:25 +0000

    This reduce functtion has been finished. This reducer will be passed all values on a given day of trading. Based on my custom TextPair object it is returning the Max.

--- NASDAQ_Reduce.java
+++ NASDAQ_Reduce.java
@@ -1,21 +1,16 @@
 package Andrija.BigData.NASDAQ_Join;
 
 import java.io.IOException;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.reduce.*;
-import com.sun.tools.javac.util.Context;
-
 import HelperTextClasses.TextPair;
 
-public class NASDAQ_Reduce extends Reducer<Text, TextPair, Text, IntWritable>{
+public class NASDAQ_Reduce extends Reducer<Text, TextPair, Text, TextPair>{
 
 	
 
    	@Override
-   	public void reduce( Text key, Iterable<TextPair>values, Context context){
+   	public void reduce( Text key, Iterable<TextPair>values, Context context) throws IOException, InterruptedException{
 
    		int maxValues = Integer.MIN_VALUE;
    		
@@ -27,34 +22,29 @@ public class NASDAQ_Reduce extends Reducer<Text, TextPair, Text, IntWritable>{
    			
    			//we need to break up the TextPair and convert the second part of the pair back into an int,
    			//in order to be able to compare the two 
-   			
-   			
    			//first we will convert the 2nd field into a string
    			// then we will convert that string into an int
    			// if we compare this int to the 2nd fields of the maxPair Textpair and it is higher in value
    			// we will replace this textPair with the new one
    			
    			String temp=a.getSecond().toString();
+   			
+   			
    			int secondFieldValue=Integer.parseInt(temp);
    			
-   			if (secondFieldValue>maxPairSecondField)
+   			if (secondFieldValue>=maxPairSecondField)
    			{
-   				
+   				maxPair = a;
    				
    			}
    			
-   			
-   			
-   			
-   			
-   			
-   			
-   			
+   			context.write(key, maxPair);
+ 	
    		}
 
-   			maxValue = Math.max(maxValue, a.get());
+   	
    		}
 
-   		context.write(key, new IntWritable(maxValue));
+   		
 
 }
