5bd5597eb6d246292e734e50b15f779f9bda038b
Andria Njoku <andrianjoku@gmail.com>
2018-11-13 17:48:03 +0000

    small changes

--- NASDAQ_Reduce.java
+++ NASDAQ_Reduce.java
@@ -11,6 +11,13 @@ public class NASDAQ_Reduce extends Reducer<Text, TextPair, Text, TextPair>{
 
    	@Override
    	public void reduce( Text key, Iterable<TextPair>values, Context context) throws IOException, InterruptedException{
+   		
+   		//deliverables: we will have a year and 365 values past through each individual reduce function, what does this mean?
+   		//we need to sort these results with the highest amount of money for each individual year at the top and the following days 
+   		// in descending order
+   		//to this will need to implement a comparator function and write to context however many of the top performers output for each year
+   		// for example if we want the top 20 performers for any company on any day of any particular year we will have to compare all values with the maximum 
+   		//values on each interation bubbling to the top and values falling outside of the collection size being dropped.
 
    		int maxValues = Integer.MIN_VALUE;
    		
